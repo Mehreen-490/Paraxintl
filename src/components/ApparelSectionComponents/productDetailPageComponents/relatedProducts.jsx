fe8bc5dc854afb34c882c909d14a0ccff3022817
era-tech=420 <eratech420@gmail.com>
2023-08-20 01:47:11 +0500

    related products added

--- src/components/ApparelSectionComponents/productDetailPageComponents/relatedProducts.jsx
+++ src/components/ApparelSectionComponents/productDetailPageComponents/relatedProducts.jsx
@@ -1,7 +1,15 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
 
 function RelatedProducts() {
-  return <div>RelatedProducts</div>;
+  return (
+    <Box width="100%">
+      <Typography fontWeight="bold" variant="h6">
+        RelatedProducts
+      </Typography>
+      <Stack direction="row"></Stack>
+    </Box>
+  );
 }
 
 export default RelatedProducts;
