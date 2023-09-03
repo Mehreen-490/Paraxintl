521d8e3a49455332def5add035d3e9e7ce296c3a
Mehreen-490 <92629441+Mehreen-490@users.noreply.github.com>
2023-09-03 23:39:05 +0500

    changed ProductPage name to DesignProductPage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ApparelPage from "./pages/ApparelPage";
 import OrderPage from "./pages/OrderPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
-import ProductPage from "./pages/ProductPage";
+import ProductPage from "./pages/DesignProductPage";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import ProductDetailPage from "./pages/ProductDetailPage";
 const theme = createTheme();
@@ -21,7 +21,7 @@ function App() {
           <Route path="/how-to-order" element={<OrderPage />} />
           <Route path="/about-us" element={<AboutPage />} />
           <Route path="/contact-us" element={<ContactPage />} />
-          <Route path="/product" element={<ProductPage />} />
+          // <Route path="/product" element={<ProductPage />} />
           <Route path="/product-detail" element={<ProductDetailPage />} />
         </Routes>
       </ThemeProvider>
--- src/components/apparelPageComponents/productCategoies.jsx
+++ src/components/apparelPageComponents/productCategoies.jsx
@@ -25,6 +25,48 @@ const ProductCategories = () => {
     newHoverStates[index] = false;
     setHoverStates(newHoverStates);
   };
+  const product_category_card_detail = [
+    {
+      id: 0,
+      title: "Dental",
+      image: "images.product_category.dental_instruments",
+    },
+    {
+      id: 1,
+      title: "Surgical",
+      image: "images.product_category.dental_instruments",
+    },
+    {
+      id: 2,
+      title: "Plastic Surgery",
+      image: "images.product_category.dental_instruments",
+    },
+    {
+      id: 3,
+      title: "Electro Surgical",
+      image: "images.product_category.dental_instruments",
+    },
+    {
+      id: 4,
+      title: "Single Use",
+      image: "images.product_category.dental_instruments",
+    },
+    {
+      id: 5,
+      title: "Gyenecology",
+      image: "images.product_category.dental_instruments",
+    },
+    {
+      id: 6,
+      title: "Beauty",
+      image: "images.product_category.dental_instruments",
+    },
+    {
+      id: 7,
+      title: "Misc.",
+      image: "images.product_category.dental_instruments",
+    },
+  ];
 
   return (
     <Box
@@ -408,3 +450,347 @@ const ProductCategories = () => {
 };
 
 export default ProductCategories;
+//  {/* Dental Instruments Box */}
+//  <Grid item xl={2.4} lg={3} md={4} sm={4} xs={6}>
+//  <Box sx={{ position: "relative", display: "inline-block" }}>
+//    {/* Dental Instruments Avatar */}
+//    <Avatar
+//      src={images.product_category.dental_instruments}
+//      sx={{
+//        width: "100%",
+//        height: "100%",
+//        borderRadius: "10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(1)}
+//      onMouseLeave={() => handleMouseLeave(1)}
+//    />
+//    <Box
+//      sx={{
+//        position: "absolute",
+//        bottom: 0,
+//        left: 0,
+//        width: "100%",
+//        height: "40px",
+//        display: "flex",
+//        alignItems: "center",
+//        justifyContent: "center",
+//        background: hoverStates[1] ? "rgba(0, 0, 0, 0.8)" : "",
+//        opacity: hoverStates[1] ? 1 : 0.7,
+//        borderRadius: "0 0 10px 10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(1)}
+//      onMouseLeave={() => handleMouseLeave(1)}
+//    >
+//      {/* Dental Instruments Typography */}
+//      <Typography
+//        sx={{
+//          color: hoverStates[1] ? "white" : "black",
+//          textAlign: "center",
+//        }}
+//      >
+//        Dental
+//      </Typography>
+//    </Box>
+//  </Box>
+// </Grid>
+// {/* Electro Surgical Instruments Box */}
+// <Grid item xl={2.4} lg={3} md={4} sm={4} xs={6}>
+//  <Box sx={{ position: "relative", display: "inline-block" }}>
+//    {/* Electro Surgical Instruments Avatar */}
+//    <Avatar
+//      src={images.product_category.electro_surgical_instruments}
+//      sx={{
+//        width: "100%",
+//        height: "100%",
+//        borderRadius: "10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(2)}
+//      onMouseLeave={() => handleMouseLeave(2)}
+//    />
+//    <Box
+//      sx={{
+//        position: "absolute",
+//        bottom: 0,
+//        left: 0,
+//        width: "100%",
+//        height: "40px",
+//        display: "flex",
+//        alignItems: "center",
+//        justifyContent: "center",
+//        background: hoverStates[2] ? "rgba(0, 0, 0, 0.8)" : "",
+//        opacity: hoverStates[2] ? 1 : 0.7,
+//        borderRadius: "0 0 10px 10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(2)}
+//      onMouseLeave={() => handleMouseLeave(2)}
+//    >
+//      {/* Electro Surgical Instruments Typography */}
+//      <Typography
+//        sx={{
+//          color: hoverStates[2] ? "white" : "black",
+//          textAlign: "center",
+//        }}
+//      >
+//        Electro Surgical
+//      </Typography>
+//    </Box>
+//  </Box>
+// </Grid>
+// {/* Surgical Instruments Box */}
+// <Grid item xl={2.4} lg={3} md={4} sm={4} xs={6}>
+//  <Box sx={{ position: "relative", display: "inline-block" }}>
+//    {/* Surgical Instruments Avatar */}
+//    <Avatar
+//      src={images.product_category.surgical_instruments}
+//      sx={{
+//        width: "100%",
+//        height: "100%",
+//        borderRadius: "10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(3)}
+//      onMouseLeave={() => handleMouseLeave(3)}
+//    />
+//    <Box
+//      sx={{
+//        position: "absolute",
+//        bottom: 0,
+//        left: 0,
+//        width: "100%",
+//        height: "40px",
+//        display: "flex",
+//        alignItems: "center",
+//        justifyContent: "center",
+//        background: hoverStates[3] ? "rgba(0, 0, 0, 0.8)" : "",
+//        opacity: hoverStates[3] ? 1 : 0.7,
+//        borderRadius: "0 0 10px 10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(3)}
+//      onMouseLeave={() => handleMouseLeave(3)}
+//    >
+//      {/* Surgical Instruments Typography */}
+//      <Typography
+//        sx={{
+//          color: hoverStates[3] ? "white" : "black",
+//          textAlign: "center",
+//        }}
+//      >
+//        Surgical
+//      </Typography>
+//    </Box>
+//  </Box>
+// </Grid>
+// {/* Plastic Surgery Instruments Box */}
+// <Grid item xl={2.4} lg={3} md={4} sm={4} xs={6}>
+//  <Box sx={{ position: "relative", display: "inline-block" }}>
+//    {/* Plastic Surgery Instruments Avatar */}
+//    <Avatar
+//      src={images.product_category.plastic_surgery_instruments}
+//      sx={{
+//        width: "100%",
+//        height: "100%",
+//        borderRadius: "10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(4)}
+//      onMouseLeave={() => handleMouseLeave(4)}
+//    />
+//    <Box
+//      sx={{
+//        position: "absolute",
+//        bottom: 0,
+//        left: 0,
+//        width: "100%",
+//        height: "40px",
+//        display: "flex",
+//        alignItems: "center",
+//        justifyContent: "center",
+//        background: hoverStates[4] ? "rgba(0, 0, 0, 0.8)" : "",
+//        opacity: hoverStates[4] ? 1 : 0.7,
+//        borderRadius: "0 0 10px 10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(4)}
+//      onMouseLeave={() => handleMouseLeave(4)}
+//    >
+//      {/* Plastic Surgery Instruments Typography */}
+//      <Typography
+//        sx={{
+//          color: hoverStates[4] ? "white" : "black",
+//          textAlign: "center",
+//        }}
+//      >
+//        Plastic Surgery
+//      </Typography>
+//    </Box>
+//  </Box>
+// </Grid>
+// {/* Gynecology Instruments Box */}
+// <Grid item xl={2.4} lg={3} md={4} sm={4} xs={6}>
+//  <Box sx={{ position: "relative", display: "inline-block" }}>
+//    {/* Gynecology Instruments Avatar */}
+//    <Avatar
+//      src={images.product_category.gynecology_instruments}
+//      sx={{
+//        width: "100%",
+//        height: "100%",
+//        borderRadius: "10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(5)}
+//      onMouseLeave={() => handleMouseLeave(5)}
+//    />
+//    <Box
+//      sx={{
+//        position: "absolute",
+//        bottom: 0,
+//        left: 0,
+//        width: "100%",
+//        height: "40px",
+//        display: "flex",
+//        alignItems: "center",
+//        justifyContent: "center",
+//        background: hoverStates[5] ? "rgba(0, 0, 0, 0.8)" : "",
+//        opacity: hoverStates[5] ? 1 : 0.7,
+//        borderRadius: "0 0 10px 10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(5)}
+//      onMouseLeave={() => handleMouseLeave(5)}
+//    >
+//      {/* Gynecology Instruments Typography */}
+//      <Typography
+//        sx={{
+//          color: hoverStates[5] ? "white" : "black",
+//          textAlign: "center",
+//        }}
+//      >
+//        Gynecology
+//      </Typography>
+//    </Box>
+//  </Box>
+// </Grid>
+// {/* Single Use Instruments Box */}
+// <Grid item xl={2.4} lg={3} md={4} sm={4} xs={6}>
+//  <Box sx={{ position: "relative", display: "inline-block" }}>
+//    {/* Single Use Instruments Avatar */}
+//    <Avatar
+//      src={images.product_category.single_use_instruments}
+//      sx={{
+//        width: "100%",
+//        height: "100%",
+//        borderRadius: "10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(6)}
+//      onMouseLeave={() => handleMouseLeave(6)}
+//    />
+//    <Box
+//      sx={{
+//        position: "absolute",
+//        bottom: 0,
+//        left: 0,
+//        width: "100%",
+//        height: "40px",
+//        display: "flex",
+//        alignItems: "center",
+//        justifyContent: "center",
+//        background: hoverStates[6] ? "rgba(0, 0, 0, 0.8)" : "",
+//        opacity: hoverStates[6] ? 1 : 0.7,
+//        borderRadius: "0 0 10px 10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(6)}
+//      onMouseLeave={() => handleMouseLeave(6)}
+//    >
+//      {/* Single Use Instruments Typography */}
+//      <Typography
+//        sx={{
+//          color: hoverStates[6] ? "white" : "black",
+//          textAlign: "center",
+//        }}
+//      >
+//        Single Use
+//      </Typography>
+//    </Box>
+//  </Box>
+// </Grid>
+// {/* Misc. Instruments Box */}
+// <Grid item xl={2.4} lg={3} md={4} sm={4} xs={6}>
+//  <Box sx={{ position: "relative", display: "inline-block" }}>
+//    {/* Misc. Instruments Avatar */}
+//    <Avatar
+//      src={images.product_category.misc_instruments}
+//      sx={{
+//        width: "100%",
+//        height: "100%",
+//        borderRadius: "10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(7)}
+//      onMouseLeave={() => handleMouseLeave(7)}
+//    />
+//    <Box
+//      sx={{
+//        position: "absolute",
+//        bottom: 0,
+//        left: 0,
+//        width: "100%",
+//        height: "40px",
+//        display: "flex",
+//        alignItems: "center",
+//        justifyContent: "center",
+//        background: hoverStates[7] ? "rgba(0, 0, 0, 0.8)" : "",
+//        opacity: hoverStates[7] ? 1 : 0.7,
+//        borderRadius: "0 0 10px 10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(7)}
+//      onMouseLeave={() => handleMouseLeave(7)}
+//    >
+//      {/* Misc. Instruments Typography */}
+//      <Typography
+//        sx={{
+//          color: hoverStates[7] ? "white" : "black",
+//          textAlign: "center",
+//        }}
+//      >
+//        Misc.
+//      </Typography>
+//    </Box>
+//  </Box>
+// </Grid>
+// {/* Beauty Instruments Box */}
+// <Grid item xl={2.4} lg={3} md={4} sm={4} xs={6}>
+//  <Box sx={{ position: "relative", display: "inline-block" }}>
+//    {/* Beauty Instruments Avatar */}
+//    <Avatar
+//      src={images.product_category.beauty_instruments}
+//      sx={{
+//        width: "100%",
+//        height: "100%",
+//        borderRadius: "10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(0)}
+//      onMouseLeave={() => handleMouseLeave(0)}
+//    />
+//    <Box
+//      sx={{
+//        position: "absolute",
+//        bottom: 0,
+//        left: 0,
+//        width: "100%",
+//        height: "40px",
+//        display: "flex",
+//        alignItems: "center",
+//        justifyContent: "center",
+//        background: hoverStates[0] ? "rgba(0, 0, 0, 0.8)" : "",
+//        opacity: hoverStates[0] ? 1 : 0.7,
+//        borderRadius: "0 0 10px 10px",
+//      }}
+//      onMouseEnter={() => handleMouseEnter(0)}
+//      onMouseLeave={() => handleMouseLeave(0)}
+//    >
+//      {/* Beauty Instruments Typography */}
+//      <Typography
+//        sx={{
+//          color: hoverStates[0] ? "white" : "black",
+//          textAlign: "center",
+//        }}
+//      >
+//        Beauty
+//      </Typography>
+//    </Box>
+//  </Box>
+// </Grid>
